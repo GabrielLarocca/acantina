c6d90826c524799fcde7e14b2fe0582db03e783f
Gabriel Larocca <glarocca@laon.com.br>
2022-10-18 14:29:37 -0300

    database update

--- DumpStructure.sql
+++ DumpStructure.sql
@@ -27,19 +27,12 @@ CREATE TABLE `admins` (
   `adm_name` varchar(255) NOT NULL,
   `adm_email` varchar(255) NOT NULL,
   `adm_password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `adm_email_UNIQUE` (`adm_email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `admins`
---
-
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cart`
@@ -50,23 +43,14 @@ DROP TABLE IF EXISTS `cart`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cart` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` bigint unsigned NOT NULL,
+  `car_user_id` bigint unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_cart_1_idx` (`user_id`),
-  CONSTRAINT `fk_cart_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_cart_1_idx` (`car_user_id`),
+  CONSTRAINT `fk_cart_1` FOREIGN KEY (`car_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart`
---
-
-LOCK TABLES `cart` WRITE;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cart_products`
@@ -87,17 +71,25 @@ CREATE TABLE `cart_products` (
   KEY `fk_cart_products_2_idx` (`product_id`),
   CONSTRAINT `fk_cart_products_1` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`),
   CONSTRAINT `fk_cart_products_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cart_products`
+-- Table structure for table `category`
 --
 
-LOCK TABLES `cart_products` WRITE;
-/*!40000 ALTER TABLE `cart_products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_products` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(45) DEFAULT NULL,
+  `cat_ativo` tinyint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `coupon_user`
@@ -121,15 +113,6 @@ CREATE TABLE `coupon_user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `coupon_user`
---
-
-LOCK TABLES `coupon_user` WRITE;
-/*!40000 ALTER TABLE `coupon_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `coupon_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `coupons`
 --
 
@@ -139,7 +122,7 @@ DROP TABLE IF EXISTS `coupons`;
 CREATE TABLE `coupons` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `cou_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cou_discount` int DEFAULT NULL,
+  `cou_discount` decimal(10,2) DEFAULT NULL,
   `cou_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `cou_initial_date` datetime NOT NULL,
   `cou_finish_date` datetime NOT NULL,
@@ -147,43 +130,8 @@ CREATE TABLE `coupons` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `coupons`
---
-
-LOCK TABLES `coupons` WRITE;
-/*!40000 ALTER TABLE `coupons` DISABLE KEYS */;
-/*!40000 ALTER TABLE `coupons` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `files`
---
-
-DROP TABLE IF EXISTS `files`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `files` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `fil_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fil_size` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `files`
---
-
-LOCK TABLES `files` WRITE;
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `order`
@@ -200,22 +148,15 @@ CREATE TABLE `order` (
   `ord_state_order` text,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `ord_state_payment` text,
+  `ord_nf` text,
   PRIMARY KEY (`id`),
   KEY `fk_order_1_idx` (`ord_cart_id`),
   KEY `fk_order_2_idx` (`ord_user_id`),
   CONSTRAINT `fk_order_1` FOREIGN KEY (`ord_cart_id`) REFERENCES `cart` (`id`),
   CONSTRAINT `fk_order_2` FOREIGN KEY (`ord_user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `order_products`
@@ -240,40 +181,26 @@ CREATE TABLE `order_products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order_products`
+-- Table structure for table `personal_access_tokens`
 --
 
-LOCK TABLES `order_products` WRITE;
-/*!40000 ALTER TABLE `order_products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_products` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `painel_orders`
---
-
-DROP TABLE IF EXISTS `painel_orders`;
+DROP TABLE IF EXISTS `personal_access_tokens`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `painel_orders` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `por_order_id` int DEFAULT NULL,
-  `por_status` varchar(45) DEFAULT NULL,
-  `por_enter_data` datetime DEFAULT NULL,
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(45) NOT NULL,
+  `tokenable_id` bigint NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_painel_orders_1_idx` (`por_order_id`),
-  CONSTRAINT `fk_painel_orders_1` FOREIGN KEY (`por_order_id`) REFERENCES `order` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `token_UNIQUE` (`token`)
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `painel_orders`
---
-
-LOCK TABLES `painel_orders` WRITE;
-/*!40000 ALTER TABLE `painel_orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `painel_orders` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -285,25 +212,18 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `pro_name` varchar(45) NOT NULL,
-  `pro_image_id` bigint unsigned DEFAULT NULL,
   `pro_description` text,
+  `pro_image_path` text,
   `pro_price` decimal(10,2) DEFAULT NULL,
+  `pro_category_id` bigint unsigned NOT NULL,
+  `pro_active` tinyint DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `pro_image_idx` (`pro_image_id`),
-  CONSTRAINT `fk_products_1` FOREIGN KEY (`pro_image_id`) REFERENCES `files` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_products_2_idx` (`pro_category_id`),
+  CONSTRAINT `fk_products_2` FOREIGN KEY (`pro_category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -317,26 +237,22 @@ CREATE TABLE `users` (
   `usr_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `usr_id_photo` bigint unsigned DEFAULT NULL,
   `usr_active` tinyint(1) NOT NULL DEFAULT '1',
   `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`),
-  KEY `users_usr_id_photo_foreign` (`usr_id_photo`),
-  CONSTRAINT `users_usr_id_photo_foreign` FOREIGN KEY (`usr_id_photo`) REFERENCES `files` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping events for database 'cantina'
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Dumping routines for database 'cantina'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -347,4 +263,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-22 19:58:13
+-- Dump completed on 2022-10-18 14:25:44
